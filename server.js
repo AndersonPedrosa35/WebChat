ec878245cd4c2e5afb6a18688ccc4e05deaa00b0
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-10-26 14:57:56 -0300

    Criando a conecção na porta 3000, iniciando variáveis de ambiente, e configurando o servidor http e configuração básica para utilização do ejs

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // Faça seu código aqui
+// DD-MM-yyyy HH:mm:ss ${nickname} ${chatMessage}
+// exemplo 09-10-2020 2:35:09 PM - Joel: Olá meu caros amigos!
+require('dotenv').config();
+const express = require('express');
+const bodyParse = require('body-parser');
+
+const app = express();
+app.use(bodyParse.json());
+const http = require('http').createServer(app);
+
+app.use('view engine', 'ejs');
+app.use('views', './views');
+
+http.listen(3000, () => {
+  console.log(`Online na porta ${}`)
+})
