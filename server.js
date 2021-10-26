d2f99b4656efa33827e4fadf44938cbd075f271b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-10-26 16:02:40 -0300

    Feito produto mínimo viável com socket.io, configuração inicial feita, e estrutura do index pronta

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 // exemplo 09-10-2020 2:35:09 PM - Joel: Olá meu caros amigos!
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParse = require('body-parser');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParse.json());
+
 const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://127.0.0.1:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 const { PORT } = process.env;
 
-app.use('view engine', 'ejs');
-app.use('views', './views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.get('/', (req, res) => res.sendFile(`${__dirname}index.ejs`));
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    console.log(`${nickname} : ${chatMessage}`);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.post('/', (_req, res, _next) => {
+  res.render('index.ejs');
+});
 
 http.listen(PORT, () => {
   console.log(`Online na porta ${PORT}`);
