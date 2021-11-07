19d7da7bbd3f1c121d4ffcc0dadc7b1e674251b4
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-07 11:40:02 -0300

    Criação das validações na criação da mensagem no banco

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,5 +1,18 @@
 const connection = require('./connection');
 
+const isValidNickname = (nickname) => {
+  if (!nickname || nickname.trim() === '') {
+    return { status: 400, message: '"nickname" is required' };
+  }
+  return true;
+};
+
+const isValidMessage = (message) => {
+  if (!message || message.trim() === '') {
+    return { status: 400, message: '"message" is required' };
+  }
+};
+
 const getAllMessages = async () => {
   const data = await connection().then((db) => db.collection('messages'));
 
@@ -7,15 +20,21 @@ const getAllMessages = async () => {
   return result;
 };
 
-const createMessage = async ({ nickname, messages }) => {
+const createMessage = async ({ nickname, message, hours }) => {
+  const validName = isValidNickname(nickname);
+  const validMessage = isValidMessage(message);
+  if (validName.message) {
+    return validName;
+  }
+  if (validMessage.message) {
+    return validMessage;
+  }
+
+  const data = await connection().then((db) => db.collection('messages'));
+  const { insertedId } = await data.insertOne({ message, nickname, timestamp: hours });
+  return { _id: insertedId, nickname, message, timestamp: hours };
 };
 
 module.exports = {
   getAllMessages,
 };
-
-{
-  message: 'Lorem ipsum',
-  nickname: 'xablau',
-  timestamp: '2021-04-01 12:00:00'
-}
